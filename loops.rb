932736bfaf0da4ac12b4869eed9141b882900240
rails -v <netagonen@gmail.com>
2016-11-30 19:59:04 +0100

    loops

---
+++ loops.rb
@@ -0,0 +1,13 @@
+def collatz(n)
+  numbers =[]
+  num_of_loops = 0
+  while n>1
+    (n%2 == 0) ? (n = n/2) : (n = (3*n)+1)
+    numbers << n
+    num_of_loops +=1
+  end
+  print numbers 
+  puts "the loop terminated after #{num_of_loops} and the largest value was #{numbers.max}"
+end
+
+collatz(6)
---
+++ methods.rb
@@ -0,0 +1,12 @@
+def add_1(number)
+  puts "add_1 was called"
+  number + 1
+end
+
+def sum (num1, num2)
+  num1 + num2
+end
+
+puts add_1(4)
+result = add_1(4)
+puts sum(4, result)
