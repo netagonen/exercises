924b27680d2d4a0ac8b6f4fb7ac1b1f56d29a944
rails -v <netagonen@gmail.com>
2016-11-30 20:08:47 +0100

    loops

--- loops.rb
+++ loops.rb
@@ -1,12 +1,13 @@
 def collatz(n)
   numbers =[]
   num_of_loops = 0
-  while n>1
+  while n>1 do
     (n%2 == 0) ? (n = n/2) : (n = (3*n)+1)
     numbers << n
     num_of_loops +=1
   end
-  print numbers 
+  print numbers
+  puts
   puts "the loop terminated after #{num_of_loops} and the largest value was #{numbers.max}"
 end
 
