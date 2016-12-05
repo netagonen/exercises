ad6a94be369f510af62a497d2d88dd9a766c83f6
rails -v <netagonen@gmail.com>
2016-12-05 10:39:26 +0100

    gessing game with while loop or recursion

---
+++ gessing_game_1.rb
@@ -0,0 +1,44 @@
+def prompting
+  while true
+    puts "> "
+    answer = gets.chomp
+    answer_as_i = answer.to_i
+    answer_as_s = answer_as_i.to_s
+    if answer == answer_as_s && answer_as_i >= 0 && answer_as_i < 10
+      return answer_as_i
+    else
+      puts "This is an ERROR, you typed #{answer}, please type a positive integer"
+    end
+  end
+end
+
+def gessing_game
+  while true
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    number = numbers.sample
+    user_num = prompting
+      if
+       user_num == number
+       puts "Amazing, you guessed right, the number was #{number}!"
+       return
+     else
+      puts "Sorry, wrong answer.. the number was #{number}. Let's try again!"
+    end
+  end
+end
+
+puts "Welcome to the worst gessing game in the world!"
+puts "I'm thinking of a number from 0 to 9. What is your guess?"
+gessing_game
+
+
+
+
+
+# def prompting
+#     puts "Let me think..."
+#   else
+#     puts "Wrong input! I got #{user_num} and we're expecting you to type a number between 0-9"
+#     prompting
+#   end
+# end
--- loops.rb
+++ loops.rb
@@ -1,14 +1,12 @@
 def collatz(n)
   numbers =[]
-  num_of_loops = 0
   while n>1 do
-    (n%2 == 0) ? (n = n/2) : (n = (3*n)+1)
+    n = n.even? ? n/2 : (3*n)+1
     numbers << n
-    num_of_loops +=1
   end
   print numbers
   puts
-  puts "the loop terminated after #{num_of_loops} and the largest value was #{numbers.max}"
+  puts "the loop terminated after #{numbers.length} and the largest value was #{numbers.max}"
 end
 
-collatz(6)
+collatz(25)
