c6386e9d35664194271997ec37ae3ac4b0f3841e
rails -v <netagonen@gmail.com>
2016-12-14 19:53:19 +0100

    fizz bazz edited

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -3,16 +3,14 @@ for n in 1..100
     n = "FizzBazz"
   elsif n%3 == 0
     n = "Fizz"
-  elsif
-     n%5 == 0
+  elsif n%5 == 0
     n = "Bazz"
   end
   puts n
 end
 
-array = (1..100).to_a
-array.each do |n|
-  case 
+(1..100).each do |n|
+  case
   when n%3 == 0 && n%5 == 0
     n = "FizzBazz"
   when n%3 == 0
@@ -21,4 +19,20 @@ array.each do |n|
     n = "Bazz"
   end
   puts n
+end
+
+1.upto(100) do |n|
+  fizz = n%3 == 0
+  buzz = n%5 == 0
+
+  puts case
+  when fizz && buzz
+    'fizzbuzz'
+  when fizz
+    'fizz'
+  when buzz
+    'buzz'
+  else n
+  end
+
 end
