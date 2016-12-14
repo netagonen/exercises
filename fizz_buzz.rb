a2bbc56fe78c16ea27ffa25310dc54c291dfec53
rails -v <netagonen@gmail.com>
2016-12-14 19:08:11 +0100

    fizz bazz edited

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -9,3 +9,16 @@ for n in 1..100
   end
   puts n
 end
+
+array = (1..100).to_a
+array.each do |n|
+  case 
+  when n%3 == 0 && n%5 == 0
+    n = "FizzBazz"
+  when n%3 == 0
+    n = "Fizz"
+  when n%5 == 0
+    n = "Bazz"
+  end
+  puts n
+end
